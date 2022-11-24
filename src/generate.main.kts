9f1c7a1f1dbdfcd03aaadb166178acd7e5d3b29f
Peter Dekkers <peter@jbaron.com>
2022-11-24 13:00:51 +0100

    new format added

--- src/generate.main.kts
+++ src/generate.main.kts
@@ -1,8 +1,9 @@
-@file:DependsOn("org.roboquant:roboquant:0.9.7-SNAPSHOT")
+@file:DependsOn("org.roboquant:roboquant:0.9.8-SNAPSHOT")
 
 import org.roboquant.common.*
 import org.roboquant.feeds.AvroFeed
 import org.roboquant.feeds.csv.CSVFeed
+import java.io.File
 import kotlin.io.path.Path
 import kotlin.io.path.div
 
@@ -11,47 +12,39 @@ fun run(pathStr: String) {
 
     val path = Path(pathStr)
 
+    fun assetBuilderImpl(file: File): Asset {
+        val symbol = file.name.removeSuffix(".us.txt").replace('-', '.')
+        return Asset(symbol)
+    }
 
     val feed = CSVFeed(path / "nasdaq stocks") {
         fileExtension = ".us.txt"
         parsePattern = "??T?OHLCV?"
-        assetBuilder = { name: String -> Asset(name.replace('-', '.')) }
+        assetBuilder = { assetBuilderImpl(it) }
     }
 
     val tmp = CSVFeed(path / "nyse stocks") {
         fileExtension = ".us.txt"
         parsePattern = "??T?OHLCV?"
-        assetBuilder = { name : String -> Asset(name.replace('-', '.')) }
+        assetBuilder = { assetBuilderImpl(it) }
     }
     feed.merge(tmp)
 
-    val sp500File = "./avro/5yr_sp500_v3.0.avro"
-    val timeframe = Timeframe.fromYears(2017, 2021)
+    val sp500File = "./avro/sp500_pricebar_v4.0.avro"
+    val timeframe = Timeframe.fromYears(2020, 2023)
     val symbols = Universe.sp500.getAssets(timeframe.start).map { it.symbol }.toTypedArray()
 
     AvroFeed.record(
         feed,
         sp500File,
+        true,
         timeframe,
-        compressionLevel = 1,
         assetFilter = AssetFilter.includeSymbols(*symbols)
     )
 
-    val smallFile = "./avro/us_small_daily_v3.0.avro"
-    AvroFeed.record(
-        feed,
-        smallFile,
-        timeframe,
-        compressionLevel = 1,
-        assetFilter = AssetFilter.includeSymbols("AAPL", "AMZN", "TSLA", "IBM", "JNJ", "JPM")
-    )
 
-    for (f in listOf(sp500File, smallFile)) {
-        val avroFeed = AvroFeed(f)
-        println("file=$f timeframe=${avroFeed.timeframe} assets=${avroFeed.assets.size}")
-    }
 }
 
-if (args.size == 0) throw Exception("No directory for data provided")
+require(args.isNotEmpty())  { "No directory for CSV files is provided" }
 val dataHome = args[0]
 run(dataHome)
